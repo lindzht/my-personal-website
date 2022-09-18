acf01f3e92d511fb4b93910c2ce8b67ccd6ebb8e
Lindsay Taylor <llindsayttaylor@gmail.com>
2022-09-18 18:07:41 -0400

    enlarged background circle, moved nav stars, added about text + image

--- script.js
+++ script.js
@@ -2,3 +2,12 @@
 // h2.textContent = "This content added by JavaScript";
 
 // document.querySelector("body").appendChild(h2);
+
+const about = document.querySelector("#about");
+const aboutText = document.querySelector("h1");
+
+about.addEventListener("mouseover", function(){
+    if (aboutText === "✴"){
+        aboutText.innerText = "test";
+    }
+})
